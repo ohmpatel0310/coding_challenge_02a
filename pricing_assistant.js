7dd93cf1d5669b44487e5aa81f077344bbea9068
Ohm Patel <ohmp3@usf.edu>
2026-01-29 17:02:30 -0500

    Created and Initilize Variables

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -1,1 +1,8 @@
 // This is for a brownie shop.
+
+let product_Name = "Chocolate Brownie"
+let costPerUnit = 0.99
+let basePrice = 1.99
+let discountRate = 0.1
+let salesTaxRate = 0.07
+let fixedMonthlyCosts = 11.95 
