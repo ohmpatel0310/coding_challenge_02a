9d81ac5c21d685520885c0658a0e76615894896b
Ohm Patel <ohmp3@usf.edu>
2026-01-29 17:05:57 -0500

    Created pricing and profiit metrics

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -1,8 +1,13 @@
 // This is for a brownie shop.
 
-let product_Name = "Chocolate Brownie"
-let costPerUnit = 0.99
-let basePrice = 1.99
-let discountRate = 0.1
-let salesTaxRate = 0.07
-let fixedMonthlyCosts = 11.95 
+let product_Name = "Chocolate Brownie";
+let costPerUnit = 0.99;
+let basePrice = 1.99;
+let discountRate = 0.1;
+let salesTaxRate = 0.07;
+let fixedMonthlyCosts = 11.95;
+
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax = discountRate * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
