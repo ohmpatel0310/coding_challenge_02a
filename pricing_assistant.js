753ef41825cd3e89a88d11a33ff6042f515f7b38
Ohm Patel <ohmp3@usf.edu>
2026-01-29 17:58:47 -0500

    Made console.logs to print to consoles

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -8,6 +8,14 @@ let salesTaxRate = 0.07;
 let fixedMonthlyCosts = 11.95;
 
 let discountedPrice = basePrice * (1 - discountRate);
-let finalPriceWithTax = discountRate * (1 + salesTaxRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
 let profitPerUnit = finalPriceWithTax - costPerUnit;
 let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0;
+
+console.log("Product Name:", product_Name);
+console.log("Discounted Price: $" + discountedPrice.toFixed(2));
+console.log("Profit Per Unit: $" + finalPriceWithTax.toFixed(2));
+console.log("Break-Even Units:", breakEvenUnits);
+console.log("Is it profitable?", isProfitablePerUnit);
+
